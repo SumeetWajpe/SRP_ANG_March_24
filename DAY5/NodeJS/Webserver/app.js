36883126b78bd4876587daba1a0f04a9af8ea36f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-20 16:45:10 +0530

    ✨ using fs.readfile to read index.html page & /products for JSON

--- DAY5/NodeJS/Webserver/app.js
+++ DAY5/NodeJS/Webserver/app.js
@@ -1,12 +1,33 @@
-const http = require("http"); // built in 
-
+const http = require("http"); // built in
+const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h2>Hello World</h2>");
+  if (req.url == "/") {
+    fs.readFile("Index.html", (err, data) => {
+      if (!err) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data); // send file contents as response to client
+      }
+    });
+    //   const readableStream = fs.createReadStream("Index.html", "utf-8");
+    //   res.statusCode = 200;
+    //   res.setHeader("Content-Type", "text/html");
+    //   readableStream.pipe(res);
+  } else if (req.url == "/products") {
+    var products = [
+      { id: 1, title: "Macbook Pro" },
+      { id: 2, title: "Camera" },
+    ];
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(products));
+  } else {
+    res.statusCode = 404;
+    res.end("Resource not found !");
+  }
 });
 
 server.listen(port, hostname, () => {
