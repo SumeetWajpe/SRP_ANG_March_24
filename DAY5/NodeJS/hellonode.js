768f719081a187a4352639f51a44cd856cb0def7
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-04-20 12:32:38 +0530

    ✨ Hello Node | BuiltIn modules | npm packages

---
+++ DAY5/NodeJS/hellonode.js
@@ -0,0 +1,5 @@
+console.log("Started!");
+setTimeout(function () {
+  console.log("Printed after 2 seconds !");
+}, 2000);
+console.log("Ended!");
---
+++ DAY5/NodeJS/modules/builtIn/app.js
@@ -0,0 +1,2 @@
+const os = require("os"); // builtin
+console.log(os.cpus().length);
